f7022d6baa15a62127b316cedcd1517b5cbbc7c9
Encrypto27 <diegoallies27@gmail.com>
2022-01-06 09:34:41 +0200

    added find

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+let person = people.find((person) => {
+    return (
+      person.name.first == "Johnathan" ||
+      person.name.first == "Tom" ||
+      person.name.first == "Jack" ||
+      person.name.first == "John" ||
+      person.name.first == "Jake" ||
+      person.name.first == "Mary"
+    );
+  });
